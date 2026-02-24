78cc253501882f4bb07fa640a00db28eafa1a2bf
Cory Gold <goldcorum@gmail.com>
2026-02-24 08:44:11 -0500

    Increment version number

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ const config = {
   expo: {
     name: 'FocusFit',
     slug: 'focusfit',
-    version: '1.1.1',
+    version: '1.1.2',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     userInterfaceStyle: 'automatic',
